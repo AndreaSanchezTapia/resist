30cbe8e45bc73319a6445e181455d549e48412ac
Andrea Sánchez-Tapia <andreasancheztapia@gmail.com>
2023-05-24 21:38:31 -0700

    primeiro teste pro brasil inteiro, downsampled para 900m de pixel. block size 3 (2700m), radius 50 (45km), vai demorar 4 horas com 15 cores.

---
+++ scripts/2_prepare_for_omniscape.R
@@ -0,0 +1,15 @@
+#downsample resistance file
+
+library(terra)
+#bra_res <- rast("scripts/2_omniscape_julia/Biomas_resistencia.tif")
+#bra_res[bra_res > 120] <- NA
+#writeRaster(bra_res, "scripts/2_omniscape_julia/Biomas_resistencia_NA.tif")
+bra_res <- rast("scripts/2_omniscape_julia/Biomas_resistencia_NA.tif")
+res(bra_res)
+
+res_kernel <- rast("scripts/2_omniscape_julia/Biomas_resistencia_kernel.tif")
+# downsamplear para 900m (factor 10)
+resampled_bra <- aggregate(bra_res, 10, cores = 15)
+plot(resampled_bra) #blz
+writeRaster(resampled_bra, "scripts/2_omniscape_julia/resistencia_resampled.tif")
+
